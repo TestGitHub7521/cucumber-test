a1421300a937c24b91450913760f2dc9d5b223ab
TestGitHub7521 <40186836+TestGitHub7521@users.noreply.github.com>
2018-11-28 21:01:21 -0500

    Delete report.js

--- target/reports/cucumber/html/report.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("health.feature");
-formatter.feature({
-  "line": 1,
-  "name": "the health can be retrieved",
-  "description": "",
-  "id": "the-health-can-be-retrieved",
-  "keyword": "Feature"
-});
-});
